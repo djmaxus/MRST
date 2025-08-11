c39722c84c514f49feaf214ac829a22734e3f28b
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-08-11 12:38:09 +0100

    Update NonLinearSolver.m

--- autodiff/ad-core/solvers/NonLinearSolver.m
+++ autodiff/ad-core/solvers/NonLinearSolver.m
@@ -139,7 +139,7 @@ classdef NonLinearSolver < handle
 
             [opt, forcesArg] = merge_options(opt, varargin{:});
             state = opt.initialGuess;
-            state = weighted_init_guess(state0,opt.initialGuess,0.4);
+            state = weighted_init_guess(state0,opt.initialGuess,0.8);
             % Merge in forces as varargin
             drivingForces = merge_options(drivingForces, forcesArg{:});
             % Prepare report-step
